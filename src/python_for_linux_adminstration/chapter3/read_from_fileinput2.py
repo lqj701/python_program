ee4b9fd1cfe2944083e08c4d27bc8b1f9d1565a3
lqj701 <hhslqj@126.com>
2019-04-21 23:28:15 +0800

    上传

---
+++ src/python_for_linux_adminstration/chapter3/read_from_fileinput2.py
@@ -0,0 +1,8 @@
+from  __future__ import print_function
+import fileinput
+
+for line in fileinput.input():
+    meta = [fileinput.filename(),fileinput.fileno(),fileinput.filelineno(),fileinput.isfirstline(),fileinput.isstdin()]
+    print(*meta, end="")
+    print("\n")
+    print(line, end="")
